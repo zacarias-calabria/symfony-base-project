138975bf50bb614f368f1b261bc095b378509ed1
Zacarías <zacarias@wichipu.com>
2023-11-13 16:29:37 +0100

    chore: Added health check HTTP request.

--- apps/api/src/Controller/HealthCheck/HealthCheckGetController.php
+++ apps/api/src/Controller/HealthCheck/HealthCheckGetController.php
@@ -6,10 +6,12 @@ namespace Techpump\Apps\API\Controller\HealthCheck;
 
 use DateTimeImmutable;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Techpump\Shared\Infrastructure\Symfony\ApiController;
 
-#[Route('/health-check', name: 'health-check_get', methods: ['GET'])]
-final class HealthCheckGetController
+#[Route('/health-check', name: 'health_check_get', methods: ['GET'])]
+final class HealthCheckGetController extends ApiController
 {
     public function __invoke(): JsonResponse
     {
@@ -17,7 +19,13 @@ final class HealthCheckGetController
             [
                 'api' => 'ok',
                 'timestamp' => (new DateTimeImmutable())->getTimestamp(),
-            ]
+            ],
+            Response::HTTP_OK
         );
+    }
+
+    protected function exceptions(): array
+    {
+        return [];
     }
 }
