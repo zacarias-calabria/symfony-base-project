98534079e8e04c71c88904493e1100deec29503a
Zacarías <zacarias@wichipu.com>
2024-01-21 09:01:55 +0100

    chore: Process Rector with the SYMFONY_64 set.

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -15,7 +16,8 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     // define sets of rules
-        $rectorConfig->sets([
-            LevelSetList::UP_TO_PHP_83
-        ]);
+    $rectorConfig->sets([
+        LevelSetList::UP_TO_PHP_83,
+        SymfonySetList::SYMFONY_64
+    ]);
 };
