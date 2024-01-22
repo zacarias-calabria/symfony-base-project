183195646fb5b708e89e8fdee1ba13f4ee2ff65b
Zacarías <zacarias@wichipu.com>
2024-01-22 19:30:24 +0100

    chore: Removing unused packages from Head app.

--- apps/head/public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Apps\Head\Kernel;
-
-require_once dirname(__DIR__) . '/../../vendor/autoload_runtime.php';
-
-return static fn (array $context): Kernel => new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
