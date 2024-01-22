3f410b3dc115aa7b12893f1cd08655bc7baac811
Zacarías <zacarias@wichipu.com>
2024-01-22 18:24:18 +0100

    chore: Applying PHP CS Fixer tool custom rules.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -12,19 +12,15 @@ return (new PhpCsFixer\Config())
 //        '@PHP83Migration' => true,
 //        '@PSR12' => true,
 //        '@Symfony' => true,
-        'global_namespace_import' => [
-            'import_classes' => false,
-            'import_constants' => false,
-            'import_functions' => false,
-        ],
-        'no_unused_imports' => true,
-        'single_line_after_imports' => true,
-        'blank_line_between_import_groups' => true,
+//        'global_namespace_import' => [
+//            'import_classes' => false,
+//            'import_constants' => false,
+//            'import_functions' => false,
+//        ],
+//        'no_unused_imports' => true,
+//        'single_line_after_imports' => true,
+//        'blank_line_between_import_groups' => true,
+        'single_quote' => true,
+        'yoda_style' => true,
     ])
     ->setFinder($finder);
-
-//phpdoc_to_comment
-//yoda_style
-//blank_line_before_statement
-//single_quote
-//phpdoc_separation
