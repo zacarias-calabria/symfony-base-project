f6ea2253685a79256c43a6883b6f7e8e36e4aaaa
Zacarías <zacarias@wichipu.com>
2024-03-07 14:04:14 +0100

    chore: Reformed the Rector file in accordance with Rector standards.

--- rector.php
+++ rector.php
@@ -6,19 +6,16 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Symfony\Set\SymfonySetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/apps',
-        __DIR__ . '/bin',
-        __DIR__ . '/features',
-        __DIR__ . '/src',
-        __DIR__ . '/tests',
-    ]);
-
-    // define sets of rules
-    $rectorConfig->sets([
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/apps',
+        __DIR__.'/bin',
+        __DIR__.'/features',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withSets([
         LevelSetList::UP_TO_PHP_83,
         SymfonySetList::SYMFONY_64,
-        SymfonySetList::SYMFONY_CODE_QUALITY
+        SymfonySetList::SYMFONY_CODE_QUALITY,
     ]);
-    };
