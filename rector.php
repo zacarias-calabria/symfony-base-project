68259c10f79588494c31bd788c81f3e4178ef4f9
Zacarías <zacarias@wichipu.com>
2024-03-07 14:16:48 +0100

    chore: Added type declaration set list to Rector file.

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 use Rector\Symfony\Set\SymfonySetList;
 
 return RectorConfig::configure()
@@ -19,4 +20,5 @@ return RectorConfig::configure()
         LevelSetList::UP_TO_PHP_83,
         SymfonySetList::SYMFONY_64,
         SymfonySetList::SYMFONY_CODE_QUALITY,
+        SetList::TYPE_DECLARATION,
     ]);
