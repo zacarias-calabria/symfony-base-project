b6fcaead0f5a1c720d727e2c2a8cf34e9f871e1d
Zacarías <zacarias@wichipu.com>
2024-03-11 13:01:39 +0100

    refactor: Remove unused method on Assert.

--- src/Shared/Domain/Assert.php
+++ src/Shared/Domain/Assert.php
@@ -7,13 +7,6 @@ namespace App\Shared\Domain;
 
 final class Assert
 {
-    public static function arrayOf(string $class, array $items): void
-    {
-        foreach ($items as $item) {
-            self::instanceOf($class, $item);
-        }
-    }
-
     public static function instanceOf(string $class, mixed $item): void
     {
         if (!$item instanceof $class) {
