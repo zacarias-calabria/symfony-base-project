b1a9f5372aa2c2cb225e906cf649288ef7bde5a5
Zacarías <zacarias@wichipu.com>
2025-02-26 23:09:05 +0100

    test: Update health check route in ApiAppAvailabilityTest

--- tests/apps/api/src/ApiAppAvailabilityTest.php
+++ tests/apps/api/src/ApiAppAvailabilityTest.php
@@ -26,6 +26,6 @@ class ApiAppAvailabilityTest extends ApiAppWebTestCase
 
     public static function checkRoutesProvider(): Generator
     {
-        yield ['GET', '/Inner/health-check'];
+        yield ['GET', '/inner/shared/health-check'];
     }
 }
